a44bd8e29e498ce57dd28b3ddd0c83f9548f469d
maxastaf <max.a.staf@gmail.com>
2023-01-19 15:53:03 +0200

    lesson #25 part1

---
+++ src/telran/reqursion/LinearRecursion.java
@@ -0,0 +1,99 @@
+package telran.reqursion;
+
+public class LinearRecursion {
+	public static long factorial(int n) {
+		long res = 0;
+		if (n < 0) {
+			res = factorial(-n);
+		} else if (n < 2) {
+			res = 1;
+		} else {
+			res = n * factorial(n - 1);
+		}
+		return res;
+	}
+	
+	public static long pow(int a, int b) {
+		long res = 0;
+		if (b < 0) {
+			throw new IllegalArgumentException();
+		} else if (b == 0) {
+			res = 1;
+		} else {
+			res = a * pow(a, b - 1);
+		}
+		return res;
+	}
+	
+	public static long power(int a, int b) {
+		// code cannot use cycles and *, / operators
+		if (b < 0) {
+			throw new IllegalArgumentException();
+		}
+		long res = 1;
+		if (b > 0) {
+			res = sumForPower(pow(a, b-1), a);
+		}
+		return res;
+	}
+	
+	private static long sumForPower(long pow, int a) {
+		long res = 1;
+		if (a < 0) {
+			res = -sumForPower(pow, -a);
+		} else if (a == 0) {
+			res = 0;
+		} else {
+			res = pow + sumForPower(pow, a - 1);
+		}
+		return res;
+	}
+
+	public static long sum(int[] ar) {
+		return sum(0, ar); 
+	}
+
+	private static long sum(int firstIndex, int[] ar) {
+		long res = 0;
+		if (firstIndex < ar.length) {
+			res = ar[firstIndex] + sum(firstIndex + 1, ar);
+		}
+		return res;
+	}
+	
+	public static long square(int x) {
+		// write function 
+		// no cycles; 
+		// no *, / operators; 
+		// no additional functions
+		// no static fields
+		long res = 0;
+		if (x < 0) {
+			res = square(-x);
+		}
+		if (x > 0) {
+			res = x + x - 1 + square(x-1);
+		}
+		return res;
+	}
+	
+	public static void reverse(int[] ar) {
+		// no cycles
+		// no static fields
+		// method reversing given array
+		reverse(0, ar.length - 1, ar);
+	}
+
+	private static void reverse(int firstIndex, int lastIndex, int[] ar) {
+		if (firstIndex < lastIndex) {
+			swap(ar, firstIndex, lastIndex);
+			reverse(firstIndex + 1, lastIndex - 1, ar);
+		}
+	}
+
+	private static void swap(int[] ar, int index1, int index2) {
+		int tmp = ar[index1];		
+		ar[index1] = ar[index2];
+		ar[index2] = tmp;
+	}
+}
---
+++ src/telran/reqursion/tests/LinearRecursionTest.java
@@ -0,0 +1,111 @@
+package telran.reqursion.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.reqursion.LinearRecursion.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class LinearRecursionTest {
+	
+	@Test
+	void reverseTest() {
+		makeReverseTest(new int[] {}, new int[] {});
+		makeReverseTest(new int[] {1, 2}, new int[] {2, 1});
+		makeReverseTest(new int[] {1, 2, 3}, new int[] {3, 2, 1});
+		makeReverseTest(new int[] {1, 2, 3, 4}, new int[] {4, 3, 2, 1});
+		makeReverseTest(new int[] {1, 2, 3, 4, 5}, new int[] {5, 4, 3, 2, 1});
+	}
+	
+	void makeReverseTest(int[] expected, int[] ar) {
+		reverse(ar);
+		assertArrayEquals(expected, ar);
+	}
+	
+	@Test
+	void squareTest() {
+		for (int i = 1; i < 10; i++) {
+			assertEquals(i*i, square(i));
+		}
+	}
+	
+	@Test
+	void sumTest() {
+		int[] array = {1, 2, 3, 6, 5, 4};
+	    assertEquals(21, sum(array));
+	    assertEquals(0, sum(new int[] {}));
+	}
+	
+	@Test
+	void powTest() {
+		assertEquals(1024, pow(2, 10));
+		assertEquals(1, pow(23213, 0));
+		assertEquals(125, pow(5, 3));
+		assertEquals(-64, pow(-4, 3));
+		assertThrowsExactly(IllegalArgumentException.class, () -> pow(-1, -1));
+	}
+	
+	@Test
+	void powerTest() {
+		assertEquals(1024, power(2, 10));
+		assertEquals(1, power(23213, 0));
+		assertEquals(125, power(5, 3));
+		assertEquals(-64, power(-4, 3));
+		assertThrowsExactly(IllegalArgumentException.class, () -> power(-1, -1));
+	}
+	
+	@Test
+	void factorialTest() {
+		assertEquals(1, factorial(0));
+		assertEquals(1, factorial(1));
+		assertEquals(2, factorial(2));
+		assertEquals(6, factorial(3));
+		assertEquals(24, factorial(4));
+		assertEquals(24, factorial(-4));
+		assertEquals(120, factorial(5));
+	}
+	
+	@Test
+	@Disabled
+	void fTest() {
+		f(6);
+	}
+	
+	void f(int a) {
+		if (a > 5) {
+			f(a - 1);
+		}
+	}
+	
+	@Test
+	@Disabled
+	void deepnesTest() {
+		// outputs 13 lines!!!
+		deepnes(1);
+	}
+	
+	void deepnes(long l) {
+		try {
+			deepnes(l + 1);
+		} catch (StackOverflowError e) {
+			System.out.println(l);
+		}
+		
+	}
+	
+	@Test
+	@Disabled
+	void deepnes1Test() {
+		// outups only one line
+		long d = deepnes1(1);
+		System.out.printf("Max deep is: %d%n", d);
+	}
+
+	private long deepnes1(long i) {
+		try {
+			return deepnes1(i + 1);
+		} catch (StackOverflowError e) {
+			return i;
+		}
+	}
+}
